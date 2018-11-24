aa385b5f88082b9c85ae607f147596c338c734d8
Eric Sims <EricSimsM@gmail.com>
2018-11-23 19:59:13 -0500

    AN929 are assemblies now, including stls

--- renamestls.sh
+++ renamestls.sh
@@ -1,3 +1,5 @@
 for f in ./*/*.stl;
-	do mv -v $f $(echo "$f" | sed -e 's/.ipt.stl/.stl/');
+	do
+        mv -v $f $(echo "$f" | sed -e 's/.ipt.stl/.stl/');
+        mv -v $f $(echo "$f" | sed -e 's/.iam.stl/.stl/');
 done;
